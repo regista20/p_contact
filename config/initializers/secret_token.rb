a09d492c6952920d4d2c5f8576644edda4dbff55
yuki <regista20ys@gmail.com>
2013-08-02 22:20:25 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PContact::Application.config.secret_token = '310ba971093f03c459fb3fd35f8b1c180a03113c2954d9b0a4d0f70c1664c0f323fee0ac1de8f9650f0c5726d4358645219f139d834680290e26d71bf23225c9'
